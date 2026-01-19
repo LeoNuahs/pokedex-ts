85d3963ff2c9333ca3f9f7212180282c1e6b60dc
Shaun <leonuahs@gmail.com>
2026-01-19 12:34:48 +0800

    feat: CH1 REPL - L4 REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,32 @@
+import { createInterface } from "readline";
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    });
+
+    rl.prompt();
+
+    rl.on("line", async (user_input) => {
+        const words = cleanInput(user_input);
+
+        if (words.length === 0) {
+            rl.prompt();
+            return;
+        }
+
+        const commandName = words[0];
+        console.log(`Your commandName was: ${commandName}`);
+        rl.prompt();
+    });
+}
+
 export function cleanInput(words: string): string[] {
     return words
+        .toLowerCase()
         .trim()
         .split(' ')
         .filter((word) => word.length > 0)
-        .map((word) => word.trim().toLowerCase());
 }
